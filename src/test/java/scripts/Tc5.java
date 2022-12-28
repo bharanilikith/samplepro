283915c250ef323f2e8db9c464bebc5818fd6d84
Bharanilikith <Bharanilikith@Bharani>
2022-12-28 08:44:29 +0530

    Hi

---
+++ src/test/java/scripts/Tc5.java
@@ -0,0 +1,12 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc5 {
+	@Test
+	public void demo() {
+		Reporter.log("hello API",true);
+	}
+
+}
